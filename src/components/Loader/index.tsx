beb000ef1d2257c5993c04a5b447d0c679c2fdb4
epburiti <50504484+epburiti@users.noreply.github.com>
2021-02-25 18:43:17 -0300

    Loader
    Loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+function Loader() {
+  return (
+    <Container>
+      <div className="lds-facebook">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </Container>
+  );
+}
+
+export default Loader;
---
+++ src/components/Loader/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: rgba(0, 0, 0, 0.1);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .lds-facebook {
+    display: inline-block;
+    position: relative;
+  }
+  .lds-facebook div {
+    display: inline-block;
+    position: absolute;
+    left: 8px;
+    width: 16px;
+    background: var(--orange);
+    animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+  }
+  .lds-facebook div:nth-child(1) {
+    left: 8px;
+    animation-delay: -0.24s;
+  }
+  .lds-facebook div:nth-child(2) {
+    left: 32px;
+    animation-delay: -0.12s;
+  }
+  .lds-facebook div:nth-child(3) {
+    left: 56px;
+    animation-delay: 0;
+  }
+  @keyframes lds-facebook {
+    0% {
+      top: 8px;
+      height: 64px;
+    }
+    50%,
+    100% {
+      top: 24px;
+      height: 32px;
+    }
+  }
+`;
